73ead6a7e9d9d3c27c26b70dddce510690e412e8
Sumukha Pk <sumukhapk46@gmail.com>
2019-01-23 07:15:24 +0530

    QueryType Done!

---
+++ Phase 2/src/QueryType/QueryType.go
@@ -0,0 +1,22 @@
+package QueryType
+
+type QueryType uint16
+
+var (
+	UNKNOWN QueryType = 0
+	A       QueryType = 1
+)
+
+func QueryTypeToInt(QT QueryType) uint16 {
+	if QT == A {
+		return 1
+	}
+	return uint16(UNKNOWN)
+}
+
+func IntToQueryType(Int int) QueryType {
+	if Int == 1 {
+		return A
+	}
+	return UNKNOWN
+}
